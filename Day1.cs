f8d4138d578c92c05b2807792e8936c0c02e6be5
Joep Schrama <joepschrama@hotmail.com>
2023-12-02 23:47:42 +0100

    Day 1 - part 1

---
+++ Day1.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2023
+{
+    public static class Day1
+    {
+        public static int Run(List<string> values)
+        {
+            return values.Sum(x => GetCalibrationValue(x));
+        }
+
+        private static int GetCalibrationValue(string value)
+        {
+            var numberList = value.Where(x => char.IsNumber(x)).Select(x => x.ToString());
+            var firstNumber = numberList.First();
+            var lastNumber = numberList.Last();
+            return int.Parse(firstNumber + lastNumber);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2023;
+
+var input = File.ReadLines("inputs/1_input.txt").ToList();
+var result = Day1.Run(input);
+
+Console.WriteLine(result);
