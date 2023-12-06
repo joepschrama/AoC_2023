c43097c4d5bdef7ae159988573ef7f439c6f5a59
Joep Schrama <joepschrama@hotmail.com>
2023-12-06 21:33:03 +0100

    Day 1 - Part 2

--- Day1.cs
+++ Day1.cs
@@ -2,6 +2,19 @@
 {
     public static class Day1
     {
+        private static IDictionary<string, int> _numbers = new Dictionary<string, int>
+        {
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9},
+        };
+
         public static int Run(List<string> values)
         {
             return values.Sum(x => GetCalibrationValue(x));
@@ -9,10 +22,21 @@
 
         private static int GetCalibrationValue(string value)
         {
-            var numberList = value.Where(x => char.IsNumber(x)).Select(x => x.ToString());
+            var numberList = FindNumbers(value);
             var firstNumber = numberList.First();
             var lastNumber = numberList.Last();
             return int.Parse(firstNumber + lastNumber);
+        }
+
+        private static IEnumerable<string> FindNumbers(string value)
+        {
+            foreach(var number in _numbers)
+            {
+                value = value.Replace(number.Key, $"{number.Key[0]}{number.Value}{number.Key[number.Key.Length-1]}");
+            }
+            return value
+                .Where(x => char.IsNumber(x))
+                .Select(x => x.ToString());
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AdventOfCode2023;
 
-var input = File.ReadLines("inputs/1_input.txt").ToList();
+var input = File.ReadLines("inputs/1_2_input.txt").ToList();
 var result = Day1.Run(input);
 
 Console.WriteLine(result);
